33551bd1f3b90e9acb049a7715212549ebcff5c7
pranavpadmane@gmail.com <pranavpadmane@gmail.com>
2025-07-20 23:13:34 +0000

    ADDED TABLES

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -1,11 +1,9 @@
+
 WITH daily_weather AS (
-
-
-SELECT
-*
-FROM {{ source('DEMO', 'WEATHER') }}
-
-
+    SELECT
+        *
+    FROM {{ source('DEMO', 'WEATHER') }}
 )
 
-SELECT* FROM DAILY_WEATHER;
+SELECT *
+FROM daily_weather;
