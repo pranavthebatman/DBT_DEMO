e8439a33b88da929716586a8ef4a9985111cece4
pranavpadmane@gmail.com <pranavpadmane@gmail.com>
2025-07-21 02:22:41 +0000

    committed daily weather data

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -1,9 +1,36 @@
-
 WITH daily_weather AS (
     SELECT
-        *
-    FROM {{ source('DEMO', 'WEATHER') }}
+        DATE(TIME) AS daily_weather, -- Renamed to avoid conflict with CTE name if any
+        weather,
+        temp,
+        pressure,
+        humidity,
+        clouds
+    FROM
+        {{ source('DEMO', 'WEATHER') }}
+    ORDER BY TIME -- LIMIT requires an ORDER BY to be deterministic
+    
+
+),
+
+daily_weather_agg as (
+    SELECT
+        daily_weather,
+        weather,
+        ROUND(avg(temp),2) as avg_temp,
+        ROUND(avg(pressure),2) as avg_pressure,
+        ROUND(avg(humidity),2) as avg_humidity,
+        ROUND (avg(clouds),2) as avg_clouds
+        
+    FROM
+        daily_weather
+    GROUP BY
+        daily_weather,
+        weather
+    qualify ROW_NUMBER() OVER (PARTITION BY daily_weather ORDER BY count(weather) desc) = 1
 )
 
-SELECT *
-FROM daily_weather;
+SELECT
+    *
+FROM
+    daily_weather_agg
